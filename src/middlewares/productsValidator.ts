1188028c62da6ad7a473d8ab454a0dca756956db
hector <hectoreddbenitez@gmail.com>
2022-04-05 20:57:01 -0300

    NEW:  nameValidator middleware

---
+++ src/middlewares/productsValidator.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import Products from '../interfaces/Products';
+
+const nameValidator = (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.body as Products;
+  if (name === undefined) return res.status(400).json({ error: 'Name is required' });
+  if (typeof name !== 'string') return res.status(400).json({ error: 'Name must be a string' });
+  if (name.length < 2) {
+    return res.status(400)
+      .json({ error: 'Name must be longer than 2 characters' }); 
+  }
+  next();
+};
+
+export default nameValidator;
