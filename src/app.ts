2b4ebb60e713685a52d19d8e6b3cedba097c060b
hector <hectoreddbenitez@gmail.com>
2022-04-05 21:18:51 -0300

    NEW: endpoint Porduct create - Req02

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import ProductsController from './controllers/Products';
+import amountValidator from './middlewares/amountValidator';
+import nameValidator from './middlewares/nameValidator';
 
 const app = express();
 
@@ -8,6 +10,6 @@ app.use(express.json());
 const productsController = new ProductsController();
 
 app.get('/products', productsController.findAll);
-app.post('/products', productsController.create);
+app.post('/products', nameValidator, amountValidator, productsController.create);
 
 export default app;
